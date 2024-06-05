a23eb51424928a6790e4c95d6b9e45b21bd3dccd
YooSangHyeone <yoosanghyeon1@gmail.com>
2024-06-05 17:04:05 +0900

    fixed/API_URL-eslint

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,8 @@
-import { Suspense } from 'react';
-import { API_URL } from '../../../(home)/page';
-import MovieInfo, { getMovie } from '../../../../components/movie-info';
-import MovieVideos from '../../../../components/movie-videos';
-import { Metadata } from 'next';
+import { Suspense } from "react";
+
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+import { Metadata } from "next";
 
 // async function getMovie(id: string) {
 //   console.log(`Fetching movies ${Date.now()}`);
